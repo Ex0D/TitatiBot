0bd3c758c7be1ccc5d9e058de170402f79311c7c
ex_ode <56847003+Ex0D@users.noreply.github.com>
2020-10-03 12:04:52 +0200

    Delete help.js

--- commands/help.js
+++
@@ -1,3 +0,0 @@
-exports.run = (client , channel , user , message , self) => {
-    client.say(channel , '!discord / !dofus / !mikuia / !zlan / !civ / !credit'); // Un for serais necessaire pour éviter de tout refaire a la main
-};
