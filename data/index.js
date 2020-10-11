4e33f12c02add238d418790621fdd59a35fc5879
Breci <culas.brice@gmail.com>
2020-10-11 15:38:50 +0200

    fixed db

--- data/index.js
+++ data/index.js
@@ -4,19 +4,40 @@ import FileSync from 'lowdb/adapters/FileSync'
 const adapter = new FileSync('database/db.json')
 const db = low(adapter)
 
-db.defaults({ commands: [] })
+db.defaults({
+    commands: [
+        {
+            type: "add-command",
+            channel: "titatitutu",
+            prefix: "add",
+            guards: {
+                role: "moderator",
+                allowlist: "ex_ode"
+            }
+        },
+        {
+            type: "remove-command",
+            channel: "titatitutu",
+            prefix: "remove",
+            guards: {
+                role: "moderator",
+                allowlist: "ex_ode"
+            }
+        },
+    ]
+})
     .write()
 
-export function getCommands(){
+export function getCommands() {
     return db.get('commands').value()
 }
 
-export function addCommand(command){
+export function addCommand(command) {
     db.get('commands').push(command).write()
 }
 
-export function removeCommand(channel, prefix){
+export function removeCommand(channel, prefix) {
     db.get('commands')
-        .remove({ channel, prefix })
+        .remove({channel, prefix})
         .write()
 }
